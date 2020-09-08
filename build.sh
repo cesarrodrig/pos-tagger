d836d24458d89ce96e323d7ed0771b360a8fe10e
Cesar Rodriguez Villanueva <cesar.rdz.v@gmail.com>
2020-09-09 00:39:09 +0200

    Add generate.py that tags sentences read from a text file
    It uses a previously trained model similar to `eval.py`.

--- build.sh
+++ build.sh
@@ -9,8 +9,12 @@ echo "=== Training Baseline ==="
 python train.py $TRAIN_FILENAME $DEV_FILENAME
 echo "=== Evaluating Baseline ==="
 python eval.py $TEST_FILENAME models/baseline__*.joblib
+echo "=== Tagging with Baseline ==="
+python generate.py $TEXT_FILENAME models/baseline__*.joblib > tags_baseline.txt
 echo ""
 echo "=== Training NLTK HMM ==="
 python train.py $TRAIN_FILENAME $DEV_FILENAME --model models/hmm_nltk.yaml
 echo "=== Evaluating NLTK HMM ==="
 python eval.py $TEST_FILENAME models/hmm_nltk__*.joblib
+echo "=== Tagging with NLTK HMM ==="
+python generate.py $TEXT_FILENAME models/hmm_nltk__*.joblib > tags_hmm_nltk.txt

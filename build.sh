72c9326a21cc2ac8598c79004e6a173569476d2e
Cesar Rodriguez Villanueva <cesar.rdz.v@gmail.com>
2020-09-08 11:07:33 +0200

    Add NLTK HMM tagger
    This PoS tagger directly trains and uses the HMM tagger offered by
    NLTK. It is slow, and accuracy is not great, but we can be sure it
    is correct before we implement our own.

--- build.sh
+++ build.sh
@@ -5,7 +5,12 @@ set -e
 flake8 *.py
 mypy --ignore-missing-imports *.py
 python download_datasets.py $TRAIN_FILENAME $DEV_FILENAME $TEST_FILENAME
-echo "=== Training ==="
+echo "=== Training Baseline ==="
 python train.py $TRAIN_FILENAME $DEV_FILENAME
-echo "=== Evaluating ==="
+echo "=== Evaluating Baseline ==="
 python eval.py $TEST_FILENAME models/baseline__*.joblib
+echo ""
+echo "=== Training NLTK HMM ==="
+python train.py $TRAIN_FILENAME $DEV_FILENAME --model models/hmm_nltk.yaml
+echo "=== Evaluating NLTK HMM ==="
+python eval.py $TEST_FILENAME models/hmm_nltk__*.joblib

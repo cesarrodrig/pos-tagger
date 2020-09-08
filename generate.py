c30f763e3dafc6439b8c96f164a280ccdbb1ac4f
Cesar Rodriguez Villanueva <cesar.rdz.v@gmail.com>
2020-09-09 00:39:09 +0200

    Add generate.py that tags sentences read from a text file
    It uses a previously trained model similar to `eval.py`.

---
+++ generate.py
@@ -0,0 +1,67 @@
+"""
+This script uses a previously trained model to predict the tags of an
+unlabeled dataset. For more details, see the function `cli` or run
+`python generate.py --help`.
+"""
+
+from typing import List
+
+import click
+import joblib
+import nltk
+from nltk import stem
+from sklearn import base
+
+nltk.download('wordnet')
+
+
+def generate(model: base.BaseEstimator, sentences: List[List[str]]) -> None:
+    """Tag the sentences with the given model.
+
+    Parameters
+    ----------
+    sentences : list
+        List of lists of strings representing the sentences to tag.
+    """
+    print(f"Tagging {len(sentences)} sentences.")
+
+    # Since the models were trained on the lemmatized version of the words,
+    # we also lemmatize them when tagging unlabeled sentences.
+    lemmatizer = stem.WordNetLemmatizer()
+
+    for sentence in sentences:
+        # Convert to the lemmatized versions
+        lemmatized = [lemmatizer.lemmatize(w.lower()) for w in sentence]
+
+        # Getting the model directly so we don't have to deal with the
+        # feature extractor that expects a list of `conllu.Sentence`.
+        tags = model.named_steps['model'].predict([lemmatized])
+
+        print("Word\tTag")
+        for w, t in zip(sentence, tags[0]):
+            print(f"{w}\t{t}")
+        print()
+
+
+@click.command(context_settings=dict(help_option_names=['-h', '--help']))
+@click.argument('text_filename')
+@click.argument('model_filename')
+def cli(text_filename: str, model_filename: str) -> None:
+    """Evaluates the performance of a model with the given dataset.
+    """
+    with open(text_filename, 'r') as f:
+        unlabeled_data = f.readlines()
+
+    model = joblib.load(model_filename)
+
+    # Convert to a list of lists of words.
+    sentences: List[List[str]] = []
+    for i in range(len(unlabeled_data)):
+        sentence = [w for w in unlabeled_data[i].strip().split(' ')]
+        sentences.append(sentence)
+
+    generate(model, sentences)
+
+
+if __name__ == '__main__':
+    cli()

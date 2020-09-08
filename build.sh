069c46d36ac947c23a3a8efd8f395333d8847976
Cesar Rodriguez Villanueva <cesar.rdz.v@gmail.com>
2020-09-08 10:43:37 +0200

    Download test file in build
    We will use this file in eval.py

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -e
 
 flake8 *.py
 mypy --ignore-missing-imports *.py
-python download_datasets.py $TRAIN_FILENAME $DEV_FILENAME
+python download_datasets.py $TRAIN_FILENAME $DEV_FILENAME $TEST_FILENAME
 python train.py $TRAIN_FILENAME $DEV_FILENAME
--- download_datasets.py
+++ download_datasets.py
@@ -12,18 +12,28 @@ import wget
 @click.command(context_settings=dict(help_option_names=['-h', '--help']))
 @click.argument('train_filename')
 @click.argument('dev_filename')
-def download(train_filename: str, dev_filename: str) -> None:
+@click.argument('test_filename')
+def download(
+    train_filename: str,
+    dev_filename: str,
+    test_filename: str,
+) -> None:
 
     train_url = """https://raw.githubusercontent.com/UniversalDependencies/\
 UD_English-GUM/master/en_gum-ud-train.conllu"""
     dev_url = """https://raw.githubusercontent.com/UniversalDependencies/\
 UD_English-GUM/master/en_gum-ud-dev.conllu"""
+    test_url = """https://raw.githubusercontent.com/UniversalDependencies/\
+UD_English-GUM/master/en_gum-ud-test.conllu"""
 
     if not os.path.isfile(train_filename):
         wget.download(train_url, train_filename)
 
     if not os.path.isfile(dev_filename):
         wget.download(dev_url, dev_filename)
+
+    if not os.path.isfile(test_filename):
+        wget.download(test_url, test_filename)
 
 
 if __name__ == "__main__":

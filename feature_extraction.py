817fd84beec46b19179ec85c7a8c7cdf1192268f
Cesar Rodriguez Villanueva <cesar.rdz.v@gmail.com>
2020-09-10 01:29:25 +0200

    Add LemmaExtractor utility class

--- feature_extraction.py
+++ feature_extraction.py
@@ -27,3 +27,32 @@ def extract_tags(sentences: List[conllu.TokenList]) -> np.array:
         labels[i] = [word['upos'] for word in sentence]
 
     return labels
+
+
+class LemmaExtractor:
+    """Class that extracts the lemmatized words from the sentences.
+    """
+
+    def fit(self,
+            sentences: List[conllu.TokenList],
+            y: np.array = None) -> 'LemmaExtractor':
+        return self
+
+    def transform(self, sentences: List[conllu.TokenList]) -> np.array:
+        """Get the lemmatized version of a word and return it as the feature.
+
+        Parameters
+        ----------
+        sentences : List[conllu.TokenList]
+            List of tokenized sentences loaded with `conllu`.
+
+        Returns
+        -------
+        np.array
+            Array of lists of strings containing the words of the sentences.
+        """
+        features = np.zeros(len(sentences), dtype='object')
+        for i, sentence in enumerate(sentences):
+            features[i] = [word['lemma'] for word in sentence]
+
+        return features
